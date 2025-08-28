313a586b2b89a611deb332e0e060135700ed16ea
simrankhan-coder <simrankhan10102@gmail.com>
2025-08-28 18:02:20 +0530

    code

---
+++ potd_28aug2025.py
@@ -0,0 +1,35 @@
+def maxOnes(arr, k):
+    res = 0
+
+    # Start and end pointer of the window
+    start = 0
+    end = 0
+
+    # Counter to keep track of zeros in current window
+    cnt = 0
+
+    while end < len(arr):
+        if arr[end] == 0:
+            cnt += 1
+
+        # Shrink the window from left if no. 
+        # of zeroes are greater than k
+        while cnt > k:
+            if arr[start] == 0:
+                cnt -= 1
+
+            start += 1
+
+        res = max(res, (end - start + 1))
+
+        # Increment the end pointer 
+        # to expand the window
+        end += 1
+
+    return res
+
+#Backend code
+if __name__ == "__main__":
+    arr = [1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1]
+    k = 2
+    print(maxOnes(arr, k))
