d9cf930d6819546040b23daa65a5c7a77cb8a3ba
simrankhan-coder <simrankhan10102@gmail.com>
2025-10-03 12:00:30 +0530

    code

---
+++ potd_29sep2025.py
@@ -0,0 +1,56 @@
+'''Maximum subarray sum 2
+You are given an array arr[] of integers and two integers a and b, You have to find the maximum possible sum of a contiguous subarray whose length is at least a and at most b.
+
+Examples:
+
+Input: arr[] = [4, 5, -1, -2, 6], a = 2, b = 4
+Output: 9
+Explanation: The subarray [4, 5] has length 2 and sum 9, which is the maximum among all subarrays of length between 2 and 4.
+
+Input: arr[] = [-1, 3, -1, -2, 5, 3, -5, 2, 2], a = 3, b = 5
+Output: 8
+Explanation: The subarray [3, -1, -2, 5, 3] has length 5 and sum 8, which is the maximum among all subarrays of length between 3 and 5.
+Constraints:
+1 ≤ arr.size() ≤ 105
+-105 ≤ arr[i] ≤ 105
+1 ≤ a ≤ b ≤ arr.size()
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(n)'''
+import math
+from collections import deque
+class Solution:
+    def maxSubarrSum(self, arr, a, b):
+        # code here
+        n = len(arr)
+        
+        if n == 0 or a > n:
+            return 0 if n == 0 else -math.inf
+
+        pref = [0] * (n + 1)
+        for i in range(1, n + 1):
+            pref[i] = pref[i-1] + arr[i-1]
+
+        dq = deque()
+        
+        maxSum = -math.inf
+        
+        for r in range(1, n + 1):
+            
+            l = r - a
+            
+            if l >= 0:
+                while dq and pref[dq[-1]] >= pref[l]:
+                    dq.pop()
+                
+                dq.append(l)
+            
+            while dq and dq[0] < r - b:
+                dq.popleft()
+            
+            if r >= a and dq:
+                current_sum = pref[r] - pref[dq[0]]
+                maxSum = max(maxSum, current_sum)
+        
+        return int(maxSum) if maxSum != -math.inf else 0
+        
